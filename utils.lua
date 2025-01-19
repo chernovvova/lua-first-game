40d7be6ee5d4b5688db39f0a1b73cfae99a9e50d
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-01-19 20:21:47 +0300

    feat: move pushRotate and pushRotateScale methods to utils

--- utils.lua
+++ utils.lua
@@ -40,3 +40,20 @@ function random(min, max)
         (love.math.random()*(max - min) + min
     )
 end
+
+
+function pushRotate(x, y, r)
+    love.graphics.push()
+    love.graphics.translate(x, y)
+    love.graphics.rotate(r or 0)
+    love.graphics.translate(-x, -y)
+end
+
+
+function pushRotateScale(x, y, r, sx, sy)
+    love.graphics.push()
+    love.graphics.translate(x, y)
+    love.graphics.rotate(r or 0)
+    love.graphics.scale(sx or 1, sy or sx or 1)
+    love.graphics.translate(-x, -y)
+end
