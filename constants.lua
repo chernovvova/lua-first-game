3a23888737edb0b82f4c4cf2810939aec14b1322
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-01-19 19:51:18 +0300

    refactor: make constants names in capital letters

--- constants.lua
+++ constants.lua
@@ -1,6 +1,7 @@
-default_color = {222, 222, 222}
-background_color = {16, 16, 16}
-ammo_color = {123, 200, 164}
-boost_color = {76, 195, 217}
-hp_color = {241, 103, 69}
-skill_point_color = {255, 198, 93}
+DEFAULT_COLOR = {222, 222, 222}
+BACKGROUND_COLOR = {16, 16, 16}
+AMMO_COLOR = {123, 200, 164}
+BOOST_COLOR = {76, 195, 217}
+HP_COLOR = {241, 103, 69}
+PROJECTILE_DEATH_COLOR = {255, 50, 0}
+SKILL_POINT_COLOR = {255, 198, 93}
